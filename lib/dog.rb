3d55d147f76fb7eb68f1466826087f8d163bbd02
AyubuAyubu <ayubu6143@gmail.com>
2022-10-19 14:04:53 +0300

    Done task

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
-
+ def name=(dog_name)
+        # instance variable are bound to an instance
+        @this_dogs_name = dog_name
+   end
+#    getter method that will return the value of the instance variable
+   def name
+    @this_dogs_name
+  end
 end
+our_dog = Dog.new
+our_dog.name = "Golden Retrievers"
+puts our_dog.name
